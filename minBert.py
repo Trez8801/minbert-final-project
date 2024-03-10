28a674c40ee18d7aeba5aebc40e4d11fecd2d677
TrezWrld <dontrezhobbz@gmail.com>
2024-03-10 18:03:28 -0400

    implented a tri-series infinite loop in minBert

--- minBert.py
+++ minBert.py
@@ -1,2 +1,6 @@
 while True:
-    print('I\'m tired')
+    print('I\'m tired\n')
+    while True:
+        print('I\'m also tired\n')
+        while True:
+            print('I\'m even more tired\n')
